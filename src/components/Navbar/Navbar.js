3b890952cb9f610a22cb6c9d146c4338a09f9ca2
Augusto Fernandez <augusfernandez23@gmail.com>
2022-12-24 19:14:23 -0300

    se creó barra de navegación

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Ecommerce</h1>
+            <div>
+                <button>Ejemplo1</button>
+                <button>Ejemplo2</button>
+                <button>Ejemplo3</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
