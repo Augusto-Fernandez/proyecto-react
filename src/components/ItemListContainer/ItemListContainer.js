3e318095313c24398ca949c0c8c60c98fcfa7616
Augusto Fernandez <augusfernandez23@gmail.com>
2023-01-16 18:17:17 -0300

    Se aplicaron cambios a css de ItemListContainer

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 import { getProducts, getProductsByCategory } from "../../asyncMock";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
-/* <h1 className="greeting">{greeting}</h1> */
 
 const ItemListContainer = ({greeting}) =>{
     const [products, setProducts] = useState([]);
@@ -24,7 +23,7 @@ const ItemListContainer = ({greeting}) =>{
     
     return(
         <div>
-            <h1>{greeting}</h1>
+            <h1 className="greeting">{greeting}</h1>
             <ItemList products={products}/>
         </div>
     );
