417907681dcecd4d05add425e96830e06f3eff62
Augusto Fernandez <augusfernandez23@gmail.com>
2023-01-17 17:00:19 -0300

    Se aplicó css a Item y ItemList

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,13 +1,14 @@
+import "./Item.css"
 import { Link } from "react-router-dom";
 
 const Item = ({id, name, img, price}) =>{
 
     return(
-        <div>
-            <h4>{name}</h4>
-            <img src={img} alt={name} style={{width: 100}}/>
-            <p>$ {price}</p>
-            <Link to={`/item/${id}`}>Ver detalles</Link> 
+        <div className="item_div">
+            <h4 className="item_name">{name}</h4>
+            <img className="item_img" src={img} alt={name} style={{width: 100}}/>
+            <p className="item_price">$ {price}</p>
+            <Link to={`/item/${id}`}><p className="item_detail">Ver detalles</p></Link> 
         </div>
     )
 }
